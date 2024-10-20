e9ea3ae4919d84e0f53f72df0629c590d13c5466
magzetic <magzetic@gmail.com>
2024-10-20 09:13:10 +0530

    Initial test

--- supabaseClient.js
+++ supabaseClient.js
@@ -1,7 +1,7 @@
 const supabaseUrl = 'https://doxaiwxzsiuvnkvnhabv.supabase.co';
 const supabaseKey =
   'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRveGFpd3h6c2l1dm5rdm5oYWJ2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjkzOTMwMTgsImV4cCI6MjA0NDk2OTAxOH0.weEWEUrUk4hYrdObLDhovH8T7_M10unDtgl4cFgvDWY';
-const supabase = createClient(supabaseUrl, supabaseKey);
+  const supabase = supabase.createClient(supabaseUrl, supabaseKey); // Accessing through the global supabase object
 export default supabase;
 
 
