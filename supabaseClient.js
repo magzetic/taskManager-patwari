754b9a8313ef39546fcc4e24e1020f8e9bc0baa9
magzetic <magzetic@gmail.com>
2024-10-20 09:10:36 +0530

    Initial test

--- supabaseClient.js
+++ supabaseClient.js
@@ -1,6 +1,14 @@
-const { createClient } = supabase;
+import { createClient } from '@supabase/supabase-js';
 const supabaseUrl = 'https://doxaiwxzsiuvnkvnhabv.supabase.co';
 const supabaseKey =
   'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRveGFpd3h6c2l1dm5rdm5oYWJ2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjkzOTMwMTgsImV4cCI6MjA0NDk2OTAxOH0.weEWEUrUk4hYrdObLDhovH8T7_M10unDtgl4cFgvDWY';
 const supabase = createClient(supabaseUrl, supabaseKey);
 export default supabase;
+
+
+
+// Replace these with your actual Supabase URL and Key
+
+// Create a Supabase client instance
+
+// Export the supabase instance for use in other modules
