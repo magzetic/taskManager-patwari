22710eee1dff8aba39175a6adf1236bc33d2d4cc
magzetic <magzetic@gmail.com>
2024-10-20 09:11:57 +0530

    Initial test

--- supabaseClient.js
+++ supabaseClient.js
@@ -1,4 +1,3 @@
-import { createClient } from '@supabase/supabase-js';
 const supabaseUrl = 'https://doxaiwxzsiuvnkvnhabv.supabase.co';
 const supabaseKey =
   'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRveGFpd3h6c2l1dm5rdm5oYWJ2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjkzOTMwMTgsImV4cCI6MjA0NDk2OTAxOH0.weEWEUrUk4hYrdObLDhovH8T7_M10unDtgl4cFgvDWY';
